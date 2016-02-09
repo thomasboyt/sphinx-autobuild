ebf0432425483e70714847ac7bbab1f1cd38da45
Thomas Boyt <thomas@ledgerx.com>
2016-02-08 20:18:26 -0500

    Add --use-polling option

--- sphinx_autobuild/__init__.py
+++ sphinx_autobuild/__init__.py
@@ -22,6 +22,7 @@ except ImportError:
 from livereload import Server
 
 from watchdog.observers import Observer
+from watchdog.observers.polling import PollingObserver
 from watchdog.events import FileSystemEventHandler
 
 
@@ -57,7 +58,7 @@ class LivereloadWatchdogWatcher(object):
     """
     File system watch dog.
     """
-    def __init__(self):
+    def __init__(self, use_polling=False):
         super(LivereloadWatchdogWatcher, self).__init__()
         self._changed = False
         # TODO: Hack.
@@ -65,7 +66,12 @@ class LivereloadWatchdogWatcher(object):
         # instance to set the file which was
         # modified. Used for output purposes only.
         self._action_file = None
-        self._observer = Observer()
+
+        if use_polling:
+            self._observer = PollingObserver()
+        else:
+            self._observer = Observer()
+
         self._observer.start()
 
         # Compatibility with livereload's builtin watcher
@@ -230,6 +236,8 @@ def get_parser():
                         help=('Specify additional directories to watch. May be'
                               ' used multiple times.'),
                         dest='additional_watched_dirs')
+    parser.add_argument('--use-polling', dest='use_polling',
+                        action='store_true', default=False)
 
     for opt, meta in SPHINX_BUILD_OPTIONS:
         if meta is None:
@@ -284,7 +292,8 @@ def main():
         portn = port_for.select_random()
 
     builder = SphinxBuilder(outdir, build_args, ignored, re_ignore)
-    server = Server(watcher=LivereloadWatchdogWatcher())
+    watcher = LivereloadWatchdogWatcher(use_polling=args.use_polling)
+    server = Server(watcher=watcher)
 
     server.watch(srcdir, builder)
     for dirpath in args.additional_watched_dirs:
